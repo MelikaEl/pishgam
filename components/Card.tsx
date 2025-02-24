65209d9125c927f870d958bb176ecc68049a91f4
MelikaEl <melika.melika8@gmail.com>
2025-02-24 10:35:01 +0330

    add gradient color to the items of the service section card

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Button } from '@/components/ui/button';
+import React from "react";
+import { Button } from "@/components/ui/button";
 
 interface CardProps {
   title: string;
@@ -14,19 +14,21 @@ const Card: React.FC<CardProps> = ({
   description,
   bulletPoints,
   buttonText,
-  
 }) => {
   return (
     <div className="bg-white rounded-lg shadow-md p-6 w-full mx-auto m-10 ">
       {/* Header */}
-      <h2 className="text-xl font-bold text-gray-800 mb-4 text-center bg-custom-gray rounded p-3">{title}</h2>
-
+      <h2 className="text-xl font-extrabold mb-4 text-center bg-custom-gray rounded p-3">
+        <span className="bg-gradient-to-r from-custom-purple to-custom-blue bg-clip-text text-transparent">
+          {title}
+        </span>
+      </h2>
       {/* Body */}
       <div>
-        <p className="text-gray-600 mb-4">{description}</p>
+        <p className="text-gray-600 mb-2">{description}</p>
 
         {/* Bullet Points */}
-        <ul className="list-disc pl-5 space-y-2 text-gray-700 p-3">
+        <ul className="list-disc pl-5 space-y-2 text-gray-700 p-4 font-bold">
           {bulletPoints.map((point, index) => (
             <li key={index}>{point}</li>
           ))}
@@ -35,9 +37,7 @@ const Card: React.FC<CardProps> = ({
 
       {/* Button */}
       <div className="mt-6 flex justify-end">
-        <Button  className="">
-          {buttonText}
-        </Button>
+        <Button className="bg-gradient-to-r from-custom-purple to-custom-blue text-white">{buttonText}</Button>
       </div>
     </div>
   );
