c1bc216a2fc2bf97604bbfae507ac74300460a1f
MelikaEl <melika.melika8@gmail.com>
2025-03-03 14:56:26 +0330

    fix the marker logo load in the map

--- components/Map.tsx
+++ components/Map.tsx
@@ -15,12 +15,14 @@ interface MapProps {
 const Map: React.FC<MapProps> = ({ latitude, longitude }) => {
   useEffect(() => {
     // Fix for default icon issues with Next.js
-    delete (L.Icon.Default.prototype as any)._getIconUrl;
-    L.Icon.Default.mergeOptions({
-      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png').default,
-      iconUrl: require('leaflet/dist/images/marker-icon.png').default,
-      shadowUrl: require('leaflet/dist/images/marker-shadow.png').default,
-    });
+    delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+});
+
   }, []);
 
   return (
