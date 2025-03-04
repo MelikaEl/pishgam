90c3c16d1845d38c033a6d435ab37604f895bf2d
MelikaEl <melika.melika8@gmail.com>
2025-03-04 10:32:28 +0330

    locate the map icon at the center of its container

--- components/Map.tsx
+++ components/Map.tsx
@@ -24,7 +24,7 @@ L.Icon.Default.mergeOptions({
   }, []);
 
   return (
-    <MapContainer center={[latitude, longitude]} zoom={13} style={{ height: '100vh', width: '100%' }}>
+    <MapContainer center={[latitude, longitude]} zoom={17} style={{ height: '100%', width: '100%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
